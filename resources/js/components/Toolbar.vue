50f98fa4cac75a55c286a639cc01c9860ebbd15e
Miaababikir <miaababikir@gmail.com>
2019-04-06 19:31:09 +0200

    setting vuetify and vue router

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <template>
+            <v-toolbar>
+                <v-toolbar-title>Miaababikir</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-toolbar-items class="hidden-sm-and-down">
+                    <v-btn flat>Forum</v-btn>
+                    <v-btn flat>Ask Question</v-btn>
+                    <v-btn flat>Category</v-btn>
+                    <v-btn flat>Login</v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    };
+</script>
+
+<style scoped></style>
